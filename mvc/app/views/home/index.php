f98e3677e503918409f45fce93ec9fd33e2c925f
Dean-Tr <deantirtas@gmail.com>
2022-10-07 22:48:02 +0700

    menyelesaikan video 6 MVC Assets

--- mvc/app/views/home/index.php
+++ mvc/app/views/home/index.php
@@ -1,1 +1,9 @@
-<h1>Selamat Datang di Website Saya</h1>
+<div class="container">
+    <div class="bg-light border p-5 mt-4">
+        <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+        <p class="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur ea libero veritatis vero incidunt nemo esse iure dicta voluptate adipisci, eligendi culpa tempore quaerat sed dolorem quam voluptates cupiditate tenetur?</p>
+        <hr class="my-4">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima alias impedit iusto, corporis esse ex eaque.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+</div>
