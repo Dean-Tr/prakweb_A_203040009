55987c247756cdec9e593970f96f4955147bef26
Dean-Tr <deantirtas@gmail.com>
2022-10-08 13:38:17 +0700

    menyelesaikan video 7 MVC Model

---
+++ mvc/app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model
+{
+    private $nama = 'Dean Tirta';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
--- mvc/app/views/home/index.php
+++ mvc/app/views/home/index.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="bg-light border p-5 mt-4">
         <h1 class="display-4">Selamat Datang di Website Saya!</h1>
-        <p class="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur ea libero veritatis vero incidunt nemo esse iure dicta voluptate adipisci, eligendi culpa tempore quaerat sed dolorem quam voluptates cupiditate tenetur?</p>
+        <p class="lead">Halo, nama saya <?= $data['nama']; ?></p>
         <hr class="my-4">
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima alias impedit iusto, corporis esse ex eaque.</p>
         <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
